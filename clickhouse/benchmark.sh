3d1c975761b6f80936129bbd979430a6e43c70a9
KinderRiven <1339764596@qq.com>
2022-09-24 11:13:26 +0000

    fix

--- clickhouse/benchmark.sh
+++ clickhouse/benchmark.sh
@@ -2,29 +2,29 @@
 
 # Install
 
-curl https://clickhouse.com/ | sh
-sudo DEBIAN_FRONTEND=noninteractive ./clickhouse install
+# curl https://clickhouse.com/ | sh
+# sudo DEBIAN_FRONTEND=noninteractive ./clickhouse install
 
 # Optional: if you want to use higher compression:
-echo "
-compression:
-    case:
-        method: zstd
-" | sudo tee /etc/clickhouse-server/config.d/compression.yaml
+# echo "
+# compression:
+#     case:
+#         method: zstd
+# " | sudo tee /etc/clickhouse-server/config.d/compression.yaml
 
-sudo clickhouse start
+# sudo clickhouse start
 
 # Load the data
+# /mnt/ebs/ClickHouse_code_reading/ClickHouse/build/programs/clickhouse-client < init.sql
+# /mnt/ebs/ClickHouse_code_reading/ClickHouse/build/programs/clickhouse-client < create.sql
 
-clickhouse-client < create.sql
+# wget --continue 'https://datasets.clickhouse.com/hits_compatible/hits.tsv.gz'
+# gzip -d hits.tsv.gz
 
-wget --continue 'https://datasets.clickhouse.com/hits_compatible/hits.tsv.gz'
-gzip -d hits.tsv.gz
-
-clickhouse-client --time --query "INSERT INTO hits FORMAT TSV" < hits.tsv
+# /mnt/ebs/ClickHouse_code_reading/ClickHouse/build/programs/clickhouse-client --time --query "INSERT INTO hits FORMAT TSV" < hits.tsv
 
 # Run the queries
 
 ./run.sh
 
-clickhouse-client --query "SELECT total_bytes FROM system.tables WHERE name = 'hits' AND database = 'default'"
+/mnt/ebs/ClickHouse_code_reading/ClickHouse/build/programs/clickhouse-client --query "SELECT total_bytes FROM system.tables WHERE name = 'hits' AND database = 'default'"
